71f45644ff5164001d79a3ea45ed5b504fd1ce5c
madfozjer <fozerion00@proton.me>
2024-09-05 20:47:30 +0200

    new file with working tailwindcss

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+console.log(__dirname);
+app.use('/static', express.static('public'));
+app.use(express.static(path.join(__dirname, "dist")))
+
+app.get('/', (req, res) => {
+    fs.readFile('dist/index.html', 'utf8', (err, html) => {
+        if (err) { console.error(err); }
+        res.send(html);
+    });
+});
+
+console.log('App is online on http://localhost:5000'); app.listen(5000);
