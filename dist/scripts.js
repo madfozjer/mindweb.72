9e346526d1c3ae212dd8475f533c5d071fdb0a09
madfozjer <fozerion00@proton.me>
2024-09-10 19:43:52 +0200

    mmm.. you can attack now

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,3 +1,18 @@
+var moves = [];
+var encounterHP = 10;
+var encounterHPbar;
+var encounterList = [pasha];
+
+var pasha = 
+{
+  hp: "10", /*make dynamic*/
+  attack: "AA"
+}
+
+window.onload = function() {
+  encounterHPbar = document.getElementById("encounter-healthbar");
+};
+
 /*move manipulation*/
 function allowDrop(ev) {
     ev.preventDefault();
@@ -9,6 +24,34 @@ function allowDrop(ev) {
   
   function drop(ev) {
     ev.preventDefault();
+    var id = ev.target.id.toString().substring(5);
     var data = ev.dataTransfer.getData("text");
     ev.target.innerHTML = data;
+    moves[id - 1] = ev.target.innerHTML;
   }
+
+  function sendMoves() {
+    for (var i = 0; i < moves.length; i++)
+    {
+      console.log(moves[i]);
+      moveReceiver(moves[i]);
+    }
+  }
+
+  /*encounter side*/
+function moveReceiver(move) {
+  switch (move) {
+    case "AA":
+      encounterHP -= 3;
+      turnEnd();
+      break;
+    case "CA+":
+      encounterHP -= 2;
+      turnEnd();
+      break;
+  }
+
+  function turnEnd() {
+    encounterHPbar.innerHTML = "<span class='text-green-500'>enemie's hp: </span>" + encounterHP;
+  }
+}
